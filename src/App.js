6be923eb8fd2caaf019a7163ee0b43c33f0034a6
Scott Allen <scott-allen@lambdastudents.com>
2020-10-26 16:31:30 -0600

    finished with MVP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
 import React from 'react';
+import TodoList from './components/TodoList.js';
+import TodoForm from './components/TodoForm.js';
+import './components/Todo.css'
+
+const todos = [
+  {
+    task: '',
+    id: '',
+    completed: false,
+  }
+]
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos: todos
+    }
+  }
+
+  handleToggleTask = (taskId) => {
+    this.setState({
+      todos:this.state.todos.map(task => {
+        if(taskId === task.id) {
+          return {
+            ...task,
+            completed: !task.completed
+          }
+        } else {
+          return task;
+        }
+      })
+    })
+    console.log('Our task id:', taskId)
+  }
+
+  handleAddTodo = (task) => {
+    this.setState({
+      todos:[...this.state.todos, {
+        task: task,
+        id: Date.now(),
+        completed: false,
+      }]
+    })
+  }
+
+  clearTasks = () => {
+    this.setState({
+      todos:this.state.todos.filter(todo => (!todo.completed))
+    })
+  }
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Git 'Er Done</h2>
+        <TodoList todo={this.state.todos} handleToggleTask={this.handleToggleTask}/>
+        <TodoForm handleAddTodo={this.handleAddTodo} handleClearSubmitTasks={this.clearTasks} />
       </div>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,17 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo from './Todo.js';
+
+const TodoList = props => {
+
+    return (
+        <div>
+            {props.todo.map(item => (
+                <Todo handleToggleTask={props.handleToggleTask} key={item.id} item={item} />
+            ))}
+        </div>
+    )
+};
+
+export default TodoList;
